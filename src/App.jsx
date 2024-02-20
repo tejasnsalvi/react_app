22b68e41fe8a17c650187263fdbd1e4ff090d071
“Tejas <tejasnsalvi@gmail.com>
2024-02-21 00:16:45 +0530

    dynamic

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,17 @@
+const reactDescriptions = ['Fundamental', 'Crucial', 'Core'];
+
+function genRandomInt(max) {
+  return Math.floor(Math.random() * (max + 1));
+}
+
 function Header() {
+  const des = reactDescriptions[genRandomInt(reactDescriptions.length-1)]
   return (
     <header>
       <img src="src/assets/react-core-concepts.png" alt="Stylized atom" />
       <h1>React Essentials</h1>
       <p>
-        Fundamental React concepts you will need for almost any app you are
-        going to build!
+        {des}  React concepts you will need for almost any app you are going to build!
       </p>
     </header>
   );
